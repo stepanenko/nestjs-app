e9171d5bfa002bb59069415e08ed3f1fa46ac3c3
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2019-05-31 16:46:52 +0300

    crud almost done

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,21 +1,38 @@
 
-import { Controller, Get, Post, Param } from '@nestjs/common';
+import { Controller, Get, Post, Body, Query, Param, Put, Delete } from '@nestjs/common';
+import { Observable, of } from 'rxjs';
+import { CreateCatDto } from './create-cat.dto';
 
 @Controller('cats')
 export class CatsController {
   @Post()
-  create(): string {
+  async create(@Body() createCatDto: CreateCatDto) {
     return 'This action adds a new cat';
   }
 
+  // @Get()
+  // async findAll(): Promise<any[]> {
+  //   return ['An array with all cats'];
+  // }
+
   @Get()
-  async findAll(): Promise<any[]> {
-    return ['An array with all cats'];
+  findAll(@Query() query: ListAllEntities): Observable<any[]> {
+    return of([`An array with all cats (limit: ${query.limit} items`]);
   }
 
   @Get(':id')
-  findOne(@Param() params): string {
-    console.log(params.id);
-    return `This action return a #${params.id} cat`;
+  findOne(@Param('id') id: string) {
+    console.log(id);
+    return `This action returns a #${id} cat`;
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto) {
+    return `This action updates a #${id} cat`;
+  }
+
+  @Delete(':id')
+  delete(@Param() id: string) {
+    return `This action removes #${id} cat`;
   }
 }
