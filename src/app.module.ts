bc83446540ae7d6ea9b6a1ade8918d1347a6ed55
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2019-06-02 12:03:56 +0300

    cats module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,32 +1,28 @@
 
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { CatsModule } from './cats/cats.module';
 import { CatsController } from './cats/cats.controller';
+import { CatsService } from './cats/cats.service';
+
+import { ItemsModule } from './items/items.module';
 import { ItemsController } from './items/items.controller';
 import { ItemsService } from './items/items.service';
-import { ItemsModule } from './items/items.module';
 
-import { MongooseModule } from '@nestjs/mongoose';
 import config from './config/keys';
-import { CatsService } from './cats/cats.service';
 
 @Module({
   imports: [
     ItemsModule,
+    CatsModule,
     MongooseModule
       .forRoot(config.mongoURI, { useNewUrlParser: true }),
   ],
-  controllers: [
-    AppController,
-    CatsController,
-    ItemsController,
-  ],
-  providers: [
-    AppService,
-    ItemsService,
-    CatsService,
-  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
